54f5ae230e63b581cd5379f1caaaf990b74d4b43
NibbledBit <me@nibbledbit.co.uk>
2021-12-27 22:35:22 +0000

    return entrance fee for the lottery

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,12 +1,20 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.6.6;
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
 
 contract Lottery {
     address payable[] public players;
-
     uint256 public gbpEntryFee;
+    AggregatorV3Interface internal ethUsdPriceFeed;
+    AggregatorV3Interface internal gbpUsdPriceFeed;
 
-    constructor() public {
+    constructor(address ethUsdPriceFeedAddress, address gbpUsdPriceFeedAddress)
+        public
+    {
         gbpEntryFee = 5 * (10**18);
+        ethUsdPriceFeed = AggregatorV3Interface(ethUsdPriceFeedAddress);
+        gbpUsdPriceFeed = AggregatorV3Interface(gbpUsdPriceFeedAddress);
     }
 
     function enter() public {
@@ -14,7 +22,25 @@ contract Lottery {
         players.push(msg.sender);
     }
 
-    function getEntranceFee() public view returns (uint256) {}
+    /// Don't send this to product because the math isn't safe
+    /// TODO:  use Safe math and workout why new versions of
+    ///        Solidity don't need safe math?
+    function getEntranceFee() public view returns (uint256) {
+        (, int256 gbpUsdPrice, , , ) = gbpUsdPriceFeed.latestRoundData();
+        (, int256 ethUsdPrice, , , ) = ethUsdPriceFeed.latestRoundData();
+
+        // convert to 18 decimals
+        uint256 adjustedEthUsdPrice = uint256(ethUsdPrice) * 10**10;
+        uint256 adjustedGbpUsdPrice = uint256(gbpUsdPrice) * 10**10;
+        //gbpEntryFee = £5
+        // £5 to USD = $6.72
+        uint256 usdEntryFee = (gbpEntryFee * 10**18) * adjustedGbpUsdPrice;
+        // ETH to USD = $4067
+        // $6.72 / $4067
+        uint256 costToEnter = usdEntryFree / adjustedEthUsdPrice;
+        //0.001652323580034423 * 10 ** 18
+        return costToEnter;
+    }
 
     function startLotter() public {}
 
